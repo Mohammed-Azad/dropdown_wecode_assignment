9a739797041bd3fd227eb60d47d6c2a1b440362c
Mohammed-Azad <mhammadazad2016@gmail.com>
2022-08-13 15:28:32 +0300

    add Number App with Api

--- lib/src/screens/number_api_screen.dart
+++ lib/src/screens/number_api_screen.dart
@@ -8,6 +8,7 @@ class Number extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: numberScreen(),
       ),
@@ -24,6 +25,7 @@ class numberScreen extends StatefulWidget {
 
 class _numberScreenState extends State<numberScreen> {
   Numberservice srvice = new Numberservice();
+  TextEditingController _numcontrol = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,31 +39,88 @@ class _numberScreenState extends State<numberScreen> {
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.all(10),
-          child: Container(
-            width: 400,
-            height: 100,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Number Input",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+        Container(
+          height: 100,
+          padding: EdgeInsets.all(10),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _numcontrol,
+                  decoration: InputDecoration(
+                    hintText: "Input Number ",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                  ),
+                ),
               ),
-            ),
+              Container(
+                  height: 65,
+                  decoration:
+                      BoxDecoration(borderRadius: BorderRadius.circular(15)),
+                  padding: EdgeInsets.all(5),
+                  child: ElevatedButton.icon(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      icon: Icon(Icons.search),
+                      label: Text("Search"))),
+            ],
           ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.topCenter,
-              width: 350,
-              height: 200,
-              child: Text("here the content"),
-            ),
-          ],
-        ),
+        _numcontrol.value.text == ''
+            ? Container(
+                child: Text(" "),
+              )
+            : Padding(
+                padding: const EdgeInsets.all(10),
+                child: Container(
+                  child: FutureBuilder<String>(
+                      future: srvice.fechNumber(_numcontrol.value.text == ''
+                          ? '0'
+                          : _numcontrol.value.text),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  child: CircularProgressIndicator(),
+                                  width: 50,
+                                  height: 50,
+                                ),
+                                SizedBox(
+                                  height: 15,
+                                ),
+                                Text("...Loading"),
+                              ],
+                            ),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Text("Error ${snapshot.error}");
+                        } else if (snapshot.data == null) {
+                          return Text("No Data");
+                        }
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  alignment: Alignment.topCenter,
+                                  width: 350,
+                                  height: 200,
+                                  child: Text("${snapshot.data}"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        );
+                      }),
+                ),
+              ),
       ],
     );
   }
--- lib/src/service/Number_service.dart
+++ lib/src/service/Number_service.dart
@@ -2,8 +2,8 @@ import 'package:http/http.dart' as http;
 import 'package:dropdown_wecode_assignment/src/screens/number_api_screen.dart';
 
 class Numberservice {
-  String number = "0";
-  Future<String> fechNumber() async {
+  
+  Future<String> fechNumber(String number) async {
     Uri _NumberApi = Uri.parse("http://numberapi.com/$number");
     http.Response _getApi =
         await http.get(_NumberApi).catchError((err) => print("$err"));
